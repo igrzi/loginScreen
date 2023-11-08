2a82a1c1b33b288d50d240462d72cbb4a69ebee4
igrzi <igorzimmer01@gmail.com>
2023-11-08 17:31:45 -0300

    feat: Implemented consultDatabase function

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/hex"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -17,8 +19,35 @@ func treatError(err error) {
 	}
 }
 
-// Given an email and a hashed password, checks on the database if the user is present, 
-// and returns a boolean value
+// Given an email and a hashed password, checks on the database
+// if the user is present, and returns a boolean value
 func consultOnDatabase(email string, hashedPassword []byte) bool {
-	...
+	var storedEmail, storedPassword string
+
+	stringedPassword := hex.EncodeToString(hashedPassword)
+
+	// Define you MySQL credentials and what database will be used
+	dsn := "root:password@tcp(localhost:3306)/loginscreenusers"
+	db, err := sql.Open("mysql", dsn)
+
+	treatError(err)
+	defer db.Close()
+
+	// Here I used a prepared statement to prevent SQL injection
+	query := "SELECT email, password FROM users WHERE email = ? AND password = ?"
+	row := db.QueryRow(query, email, stringedPassword)
+
+	err = row.Scan(&storedEmail, &storedPassword)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// No record found, indicating invalid login.
+			return false
+		}
+		treatError(err)
+		return false
+	}
+
+	// Matching record found, indicating a valid login.
+	return true
 }
